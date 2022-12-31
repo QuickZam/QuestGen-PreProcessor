c523e1436bc4b75dbe99cbac0560b81b4ee03c7a
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-12-31 15:07:17 +0530

    added all in one function

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import requests, re 
+import requests, re, os 
 from flask import Flask 
-from logger import logger 
 from pytube import extract
 import banana_dev as banana
 from doctr.io import DocumentFile  
@@ -9,8 +8,6 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 app = Flask(__name__)
-API_KEY = "ec49909f-3d2f-4044-8882-535e3ce8a383"
-MODEL_KEY = "7734639e-bcae-41f6-b7b9-47a9cbba26e1"
 model = ocr_predictor(pretrained=True)   
 
 
@@ -35,33 +32,72 @@ def yt_text(link:str) -> str:
         video_id = extract.video_id(link)
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
         text = ' '.join([ i['text'].replace('\n', '') for i in transcript])
-        text = re.sub('[^a-zA-Z0-9]', ' ', text)
+        # text = re.sub('[^a-zA-Z0-9]', ' ', text)
         # text = request_api(text)
-        logger.info(f"Transcript found: \n\n{text}")
+        # logger.info(f"Transcript found: \n\n{text}")
 
         return text
     except: 
-        logger.info(f"Transcript not found for this link: {link}, so passing to 🍌")
+        # logger.info(f"Transcript not found for this link: {link}, so passing to 🍌")
         payload = {'link': link}
         out = banana.run(API_KEY, MODEL_KEY, payload)
-        logger.info(f"Transcript from the 🍌: \n\n{out}")
+        # logger.info(f"Transcript from the 🍌: \n\n{out}")
 
         return out 
 
 def file_txt(file) -> str: 
-    if file.name.endswith('.pdf'): 
-      doc = DocumentFile.from_pdf(file.read())
-    elif file.name.endswith(('.png', '.jpeg', 'jpg')): 
-      doc = DocumentFile.from_images(file.read()) 
+    if file.endswith('.pdf'): 
+      doc = DocumentFile.from_pdf(file)
+    elif file.endswith(('.png', '.jpeg', 'jpg')): 
+      doc = DocumentFile.from_images(file) 
     else: 
       text = 'Please provide pdf or img format'
       return text 
 
     result = model(doc)
     text = result.render()
+    text = text.replace('\n', '')
 
     return text
 
+  
+def api_giver(text:str, condition='TrueOrFalse'): 
+    if condition == 'TrueOrFalse': 
+        api_key = "ec49909f-3d2f-4044-8882-535e3ce8a383"
+        model_key = "389eaf12-4801-4673-bd57-52140c4cc90c"
+        payload = {"text": text}
+        out = banana.run(api_key, model_key, payload)
+        out = out['modelOutputs'][0]['html']
 
-if __name__ == "__main__": 
-  app.run()
+        return out 
+
+    if condition == 'MCQ': 
+        api_key = "ec49909f-3d2f-4044-8882-535e3ce8a383"
+        model_key = "ddd49032-854c-4bb3-a368-9fd5b8c85646"
+
+        payload = {"text": text, "condition" : False}
+        out = banana.run(api_key, model_key, payload)
+        out = out['modelOutputs'][0]['output'][0]
+        
+        return out
+      
+ def exectuter(ins:str, condition:str, type:str): 
+    if type == 'TEXT': 
+      out = api_giver(text = ins, condition = condition) 
+     
+    if type == 'VIDEO': 
+      text = yt_text(ins) 
+      out = api_giver(text = text, condition = condition) 
+      
+    if type == 'PDF': 
+      text = file_txt(ins)
+      out = api_giver(text = in, condition = condition) 
+      
+    return out 
+      
+    
+    
+    
+    
+    
+    
